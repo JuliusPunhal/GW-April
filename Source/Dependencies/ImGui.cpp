f438eea529735bfed1d49cd7d840061562d783c0
Julius Punhal <julius@punhal.de>
2020-12-01 12:00:00 +0100

    Add submodule ImGui

---
+++ Source/Dependencies/ImGui.cpp
@@ -0,0 +1,2 @@
+
+#include "Dependencies/ImGui.hpp"
---
+++ Source/Dependencies/ImGui.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#pragma warning( push, 0 )
+#include "imgui.h"
+#include "backends/imgui_impl_dx9.h"
+#include "backends/imgui_impl_win32.h"
+#pragma warning( pop )
